a7abe75f0ba7383babbf172ba68f579951c3c070
ntthuyvy73 <ntthuyvy73@gmail.com>
2024-05-21 14:30:30 +0700

    commit

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,14 @@
 import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
+// export async function generateStaticParams() {
+//     return [
+//         { signIn: [] }, // Default route
+//         { signIn: ["example1"] }, // Example route
+//         // Thêm các tham số khác nếu cần
+//     ];
+// }
+
 const Page = () => {
     return <SignIn />;
 };
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { SignUp } from "@clerk/nextjs";
 
+// export async function generateStaticParams() {
+//     return [
+//         { signUp: [] }, // Default route
+//         { signUp: ["example2"] }, // Example route
+//         // Thêm các tham số khác nếu cần
+//     ];
+// }
+
 const Page = () => {
     return <SignUp />;
 };
